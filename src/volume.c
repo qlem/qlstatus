f2d0df20c883780eea4b629b06c58fa83d188408
qlem <clement.dommerc@gmail.com>
2020-11-11 14:49:04 +0100

    error handling and fix memory leak

--- src/volume.c
+++ src/volume.c
@@ -9,13 +9,18 @@
 // Based from sources of pa_volume_snprint() function
 #define VOLUME(avg) (((uint64_t)avg * 100 + (uint64_t)PA_VOLUME_NORM / 2) / (uint64_t)PA_VOLUME_NORM)
 
+void        free_pulse_resources(t_pulse *pulse) {
+    pa_context_disconnect(pulse->context);
+    pa_context_unref(pulse->context);
+    pa_threaded_mainloop_stop(pulse->mainloop);
+    pa_threaded_mainloop_free(pulse->mainloop);
+}
+
 void            free_volume(void *data) {
     t_module    *module = data;
     t_pulse     *pulse = module->data;
 
-    pa_context_disconnect(pulse->context);
-    pa_threaded_mainloop_stop(pulse->mainloop);
-    pa_threaded_mainloop_free(pulse->mainloop);
+    free_pulse_resources(pulse);
 }
 
 void        sink_info_cb(pa_context *context, const pa_sink_info *info,
@@ -50,13 +55,36 @@ void                    *pulse_connect(void *data) {
     t_pulse             *pulse = data;
     pa_mainloop_api     *mloop_api;
 
-    pulse->mainloop = pa_threaded_mainloop_new();
-    mloop_api = pa_threaded_mainloop_get_api(pulse->mainloop);
-    pulse->context = pa_context_new(mloop_api, PULSE_APP_NAME);
+    if ((pulse->mainloop = pa_threaded_mainloop_new()) == NULL) {
+        fprintf(stderr, "Call to pa_threaded_mainloop_new() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    if ((mloop_api = pa_threaded_mainloop_get_api(pulse->mainloop)) == NULL) {
+        fprintf(stderr, "Call to pa_threaded_mainloop_get_api() failed\n");
+        pa_threaded_mainloop_free(pulse->mainloop);
+        exit(EXIT_FAILURE);
+    }
+    if ((pulse->context = pa_context_new(mloop_api, PULSE_APP_NAME)) == NULL) {
+        fprintf(stderr, "Call to pa_context_new() failed\n");
+        pa_threaded_mainloop_free(pulse->mainloop);
+        exit(EXIT_FAILURE);
+    }
     pa_context_set_state_callback(pulse->context, context_state_cb, pulse);
-    pa_context_connect(pulse->context, NULL, PA_CONTEXT_NOFAIL |
-                       PA_CONTEXT_NOAUTOSPAWN, NULL);
-    pa_threaded_mainloop_start(pulse->mainloop);
+    if (pa_context_connect(pulse->context, NULL, PA_CONTEXT_NOFAIL |
+                           PA_CONTEXT_NOAUTOSPAWN, NULL) < 0) {
+        fprintf(stderr, "Call to pa_context_connect() failed\n");
+        pa_context_unref(pulse->context);
+        pa_threaded_mainloop_free(pulse->mainloop);
+        exit(EXIT_FAILURE);
+
+    }
+    if (pa_threaded_mainloop_start(pulse->mainloop) < 0) {
+        fprintf(stderr, "Call to pa_threaded_mainloop_start() failed\n");
+        pa_context_disconnect(pulse->context);
+        pa_context_unref(pulse->context);
+        pa_threaded_mainloop_free(pulse->mainloop);
+        exit(EXIT_FAILURE);
+    }
     while (pulse->connected == 0) {
         pa_threaded_mainloop_lock(pulse->mainloop);
         pa_threaded_mainloop_wait(pulse->mainloop);
@@ -70,15 +98,20 @@ void                    *run_volume(void *data) {
     t_pulse             *pulse = module->data;
     pa_operation        *op = NULL;
 
-    // TODO error handling
     pa_threaded_mainloop_lock(pulse->mainloop);
     op = pa_context_get_sink_info_by_name(pulse->context, pulse->sink,
                                           &sink_info_cb, module);
+    if (op == NULL) {
+        fprintf(stderr, "Call to pa_context_get_sink_info_by_name() failed\n");
+        pa_threaded_mainloop_unlock(pulse->mainloop);
+        free_pulse_resources(pulse);
+        exit(EXIT_FAILURE);
+    }
     while (pa_operation_get_state(op) == PA_OPERATION_RUNNING) {
         pa_threaded_mainloop_wait(pulse->mainloop);
     }
+    pa_operation_unref(op);
     pa_threaded_mainloop_unlock(pulse->mainloop);
-    free(op);
     return NULL;
 }
 
